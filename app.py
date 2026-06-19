c0c0b038905552a156c84c5fb5358890adea22bf
mehdifff <mehdii.nft@gmail.com>
2026-06-19 03:36:22 +0330

    Update app.py

--- app.py
+++ app.py
@@ -186,4 +186,6 @@ def retry_download(dl_id):
 
 if __name__ == '__main__':
     init_db()
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    import os
+port = int(os.environ.get('PORT', 5000))
+app.run(host='0.0.0.0', port=port, debug=False)
